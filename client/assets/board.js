68947ab526890f3d053c8dd66be861d8c0bee472
fsaiz97 <felixsaiz97@gmail.com>
2022-10-24 11:04:00 +0100

    Change form elements Add form submission functionality

--- client/assets/board.js
+++ client/assets/board.js
@@ -17,6 +17,35 @@ function createPostElement (data) {
     return post;
 }
 
+document.getElementById("post-form").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const form = new FormData(e.target);
+
+    const options = {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+
+        body: JSON.stringify({
+            title: form.get("post-title"),
+            content: form.get("post-content"),
+            sender_id: form.get("post-sender_id"),
+            recipient_id: form.get("post-recipient_id"),
+        })
+    }
+
+    const response = await fetch("http://localhost:4000/posts", options);
+    const post = await response.json();
+    console.log(post);
+
+    if (response.status === 201) {
+        window.location.reload();
+    }
+})
+
 async function loadPosts () {
     const response = await fetch("http://localhost:4000/posts"); // TODO Make port dynamic
     const posts = await response.json();
