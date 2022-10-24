58f93362ece2d4c88d838532d5bb260b2e07d10d
fsaiz97 <felixsaiz97@gmail.com>
2022-10-24 15:11:42 +0100

    Update token model Add get token by token method

--- api/models/token.js
+++ api/models/token.js
@@ -27,8 +27,13 @@ class Token {
         }
     }
 
-    static getOneByToken(token) {
-        
+    static async getOneByToken(token) {
+        const response = await db.query("SELECT * FROM token WHERE token = $1", [token]);
+        if (response.rows.length != 1) {
+            throw new Error("Unable to locate token.");
+        } else {
+            return new Token(response.rows[0]);
+        }
     }
 }
 
