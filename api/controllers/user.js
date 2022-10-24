9640914b7bbe000358b31e6995f0c0e59cc197d4
fsaiz97 <felixsaiz97@gmail.com>
2022-10-24 14:35:22 +0100

    Add users/login route

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -29,17 +29,23 @@ async function show (req, res) {
     }
 };
 
-// async function destroy (req, res) {
-//     try {
-//         const id = parseInt(req.params.id);
-//         const post = await Post.getOneById(id);
-//         const result = await post.destroy();
-//         res.status(204).end();
-//     } catch (err) {
-//         res.status(404).json({"error": err.message})
-//     }
-// };
+async function login (req, res) {
+    try {
+        const user = await User.getOneByUsername(req.body.username)
+
+        const authenticated = await bcrypt.compare(req.body.password, user["password"]);
+
+        if (!authenticated) {
+            throw new Error("Incorrect credentials.");
+        } else {
+            res.status(200).json({authenticated: true});
+        }
+
+    } catch (err) {
+        res.status(403).json({"error": err.message})
+    }
+}
 
 module.exports = {
-    register, show
+    register, show, login
 }
--- api/routers/user.js
+++ api/routers/user.js
@@ -6,5 +6,6 @@ const userRouter = Router();
 
 userRouter.post("/register", userController.register);
 userRouter.get("/:id", userController.show);
+userRouter.post("/login", userController.login)
 
 module.exports = userRouter;
