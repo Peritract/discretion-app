d619e1896112fb8fdd53aeaded5f2a3dde93c58c
Peritract <dan@getfutureproof.co.uk>
2022-11-30 22:11:38 +0000

    Blank setup

--- api/database/setup.sql
+++ api/database/setup.sql
@@ -4,7 +4,5 @@ CREATE TABLE post (
     post_id INT GENERATED ALWAYS AS IDENTITY,
     title VARCHAR (100) NOT NULL,
     content VARCHAR (500) NOT NULL,
-    sender_id INT NOT NULL,
-    recipient_id INT NOT NULL,
     PRIMARY KEY (post_id)
 );
